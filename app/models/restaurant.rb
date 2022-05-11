7db1a4bcf7e7f0d771ea45bde889726ade2efe39
Devin Chong <dvnchong88@gmail.com>
2022-05-11 14:47:52 +0900

    updating

---
+++ app/models/restaurant.rb
@@ -0,0 +1,8 @@
+class Restaurant < ApplicationRecord
+  validates :name, :address, :phone_number, :category, presence: true
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian), message: "Must be valid category"}
+  has_many :reviews, dependent: :destroy
+  # def Category
+  #   %w(chinese italian japanese french belgian)
+  # end
+end
---
+++ db/migrate/20220511025251_rename_phonenumber_to_phone_number_in_restaurants.rb
@@ -0,0 +1,5 @@
+class RenamePhonenumberToPhoneNumberInRestaurants < ActiveRecord::Migration[6.1]
+  def change
+    rename_column :restaurants, :phonenumber, :phone_number
+  end
+end
