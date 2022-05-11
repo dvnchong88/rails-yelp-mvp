54edbfdc81ff9a92689f7b614b407e11bc40a394
Devin Chong <dvnchong88@gmail.com>
2022-05-11 16:58:58 +0900

    couldn't make the reviews work

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,5 @@
 class Restaurant < ApplicationRecord
   validates :name, :address, :phone_number, :category, presence: true
-  validates :category, inclusion: { in: %w(chinese italian japanese french belgian), message: "Must be valid category"}
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian], message: "Must be valid category"}
   has_many :reviews, dependent: :destroy
-  # def Category
-  #   %w(chinese italian japanese french belgian)
-  # end
 end
